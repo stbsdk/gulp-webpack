e7b71fd248af38dd368bdb5d47bdbc58198398a9
DarkPark <darkpark@pisem.net>
2016-01-30 16:33:01 +0200

    rework spa shadowing

--- config.js
+++ config.js
@@ -7,19 +7,5 @@
 
 'use strict';
 
-var path   = require('path'),
-    extend = require('extend'),
-    config = require('spa-gulp-webpack/config');
-    //entry  = path.resolve(path.join(config.default.source, 'js', 'main.js'));
-
-
-// base config
-// each profile inherits all options from the "default" profile
-module.exports = extend(true, {}, config, {
-    develop: {
-        source: [
-            'stb-develop',
-            config.default.source
-        ]
-    }
-});
+// public
+module.exports = require('spa-gulp-webpack/config');
