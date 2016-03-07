cef1a14dab9a7ee95e01d14fe303b41182bf08c3
DarkPark <darkpark@pisem.net>
2016-03-08 00:26:29 +0200

    add app metrics.js alias

--- config.js
+++ config.js
@@ -5,5 +5,14 @@
 
 'use strict';
 
+var path   = require('path'),
+    config = require('spa-plugin-webpack/config');
+
+
+// add alias to metrics.js file in js root
+config.release.webpack.resolve.alias['app:metrics'] = path.join(process.cwd(), config.release.source, 'metrics.js');
+config.develop.webpack.resolve.alias['app:metrics'] = path.join(process.cwd(), config.develop.source, 'metrics.js');
+
+
 // public
-module.exports = require('spa-plugin-webpack/config');
+module.exports = config;
